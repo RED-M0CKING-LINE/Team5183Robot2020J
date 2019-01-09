f469863df2abcae957c32ad4ecacca31ca6a3870
Ethan Ashley <redmockingline@gmail.com>
2019-01-09 11:56:31 -0500

    Added Buttons to Xbox.java

--- Team5183Robot2019J/src/main/java/frc/robot/functions/Xbox.java
+++ Team5183Robot2019J/src/main/java/frc/robot/functions/Xbox.java
@@ -1,15 +1,27 @@
 package frc.robot.functions;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class Xbox {
-    private XboxController Controller = new XboxController(0);
+    private XboxController ctrl = new XboxController(0);
 	
-    public double L_X_STICK = Controller.getRawAxis(0); // Left stick horizontal
-    public double L_Y_STICK = Controller.getRawAxis(1); // Left stick vertical
-    public double L_TRIG = Controller.getRawAxis(2); // Left trigger
-    public double R_TRIG = Controller.getRawAxis(3); // Right trigger
-    public double R_X_STICK = Controller.getRawAxis(4); // Right stick horizontal
-    public double R_Y_STICK = Controller.getRawAxis(5); // Right stick vertical
+    public double 
+    L_X_STICK = ctrl.getRawAxis(0), // Left stick horizontal
+    L_Y_STICK = ctrl.getRawAxis(1), // Left stick vertical
+    L_TRIG = ctrl.getRawAxis(2), // Left trigger
+    R_TRIG = ctrl.getRawAxis(3), // Right trigger
+    R_X_STICK = ctrl.getRawAxis(4), // Right stick horizontal
+    R_Y_STICK = ctrl.getRawAxis(5); // Right stick vertical
+
+    public boolean 
+    getAState = ctrl.getAButton(),
+    getBState = ctrl.getBButton(),
+    getXState = ctrl.getXButton(),
+    getYState = ctrl.getYButton(),
+    getBackState = ctrl.getBackButton(),
+    getStartState = ctrl.getStartButton(),
+    getLBumperState = ctrl.getBumper(Hand.kLeft),
+    getRBumperState = ctrl.getBumper(Hand.kRight);
     
 }
