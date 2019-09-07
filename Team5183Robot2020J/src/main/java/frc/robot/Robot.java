fd8d5d19c98d81406db64b0c180a3eafbb5a4f38
Ethan Ashley <redmockingline@gmail.com>
2019-09-07 15:41:36 -0400

    Made Tabs Consistant and added togglable Intake

--- Team5183Robot2020J/src/main/java/frc/robot/Robot.java
+++ Team5183Robot2020J/src/main/java/frc/robot/Robot.java
@@ -19,61 +19,63 @@ import frc.robot.subsystems.Intake;
 
 /** This is where the main program flow is controled. */
 public class Robot extends TimedRobot {
-  private static Xbox ctrl = new Xbox(RobotMap.CONTROLLER1);
+    private static Xbox ctrl = new Xbox(RobotMap.CONTROLLER1);
 
-  //TODO MAKE A CAMERA SWITCHER
-  //Thread CameraSwitch = new Thread(() -> {});
+    //TODO MAKE A CAMERA SWITCHER
+    //Thread CameraSwitch = new Thread(() -> {});
 
-  @Override
-  public void robotInit() {
-    DriveTrain.inititalize();
-    Intake.initialize();
-    DriversStation.initialize();
-    CameraServer.getInstance().startAutomaticCapture();
-  }
-
-  @Override
-  public void robotPeriodic() {
-    DriveTrain.periodic();
-    Intake.periodic();
-    DriversStation.update();
-  }
-
-  @Override
-  public void autonomousInit() {operatorDrive();} // only to get that extra little bit of movement time :thumbs_up: (20ms ish)
-
-  @Override
-  public void autonomousPeriodic() {operatorDrive();}
-
-  @Override
-  public void teleopInit() {
-    DriveTrain.stop();  // incase the robot crashes while switching, or takes too long, the bot will stop.
-  }
-
-  @Override
-  public void teleopPeriodic() {operatorDrive();}
-
-  @Override
-  public void testInit() {}
-
-  @Override
-  public void testPeriodic() {}
-
-  /** This is the method that enables full bot operation.
-   * This exists to avoid duplicate code in teleopPeriodic and autonomousPeriodic
-   * because the 2019 season allowed manual control during the autonomous period */
-  private static void operatorDrive() {
-    DriveTrain.teleopDrive();
-    
-    if(ctrl.getAState()) {
-      Intake.in();
-      DriveTrain.teleopDrive();
-    } else if (ctrl.getXState()) {
-      Intake.out();
-      DriveTrain.teleopDrive();
-    } else {
-      Intake.stop();
-      DriveTrain.teleopDrive();
+    @Override
+    public void robotInit() {
+        DriveTrain.inititalize();
+        Intake.initialize();
+        DriversStation.initialize();
+        CameraServer.getInstance().startAutomaticCapture();
     }
-  }
+  
+    @Override
+    public void robotPeriodic() {
+        DriveTrain.periodic();
+        Intake.periodic();
+        DriversStation.update();
+    }
+  
+    @Override
+    public void autonomousInit() {operatorDrive();} // only to get that extra little bit of movement time :thumbs_up: (20ms ish)
+  
+    @Override
+    public void autonomousPeriodic() {operatorDrive();}
+  
+    @Override
+    public void teleopInit() {
+        DriveTrain.stop();  // incase the robot crashes while switching, or takes too long, the bot will stop.
+    }
+  
+    @Override
+    public void teleopPeriodic() {operatorDrive();}
+  
+    @Override
+    public void testInit() {}
+  
+    @Override
+    public void testPeriodic() {}
+  
+    /** This is the method that enables full bot operation.
+     * This exists to avoid duplicate code in teleopPeriodic and autonomousPeriodic
+     * because the 2019 season allowed manual control during the autonomous period */
+    private static void operatorDrive() {
+        DriveTrain.teleopDrive();
+
+        if(RobotMap.INTAKE_ENABLED) {
+            if(ctrl.getAState()) {
+                Intake.in();
+                DriveTrain.teleopDrive();
+            } else if (ctrl.getXState()) {
+                Intake.out();
+                DriveTrain.teleopDrive();
+            } else {
+                Intake.stop();
+                DriveTrain.teleopDrive();
+            }
+        }
+    }
 }
--- Team5183Robot2020J/src/main/java/frc/robot/RobotMap.java
+++ Team5183Robot2020J/src/main/java/frc/robot/RobotMap.java
@@ -8,37 +8,41 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Compressor;
 
 public class RobotMap {
-	// Servos
+    // Subsystem Settings
+    public static final boolean
+    INTAKE_ENABLED = false;
+    
+    // Servos
 
-	// Motors
-	public static Spark
-	MOTORS_L = new Spark(0), //Left side drive motors
-	MOTORS_R = new Spark(1), //Right side drive motors
-	INTAKE_L = new Spark(2), //Left side intake motor
-	INTAKE_R = new Spark(3); //Right side intake motor
-	
-	// Encoders
+    // Motors
+    public static Spark
+    MOTORS_L = new Spark(0), //Left side drive motors
+    MOTORS_R = new Spark(1), //Right side drive motors
+    INTAKE_L = new Spark(2), //Left side intake motor
+    INTAKE_R = new Spark(3); //Right side intake motor
+    
+    // Encoders
 
-	// Sensors
-	public static ADIS16448_Sensor SENSOR_HAT = new ADIS16448_Sensor(); // This HAT has a many, many sensors on it
-	
-	// Pneumatics - give them some slack, their just kinda bloated
-	public static DoubleSolenoid CLIMB_L = new DoubleSolenoid(0, 1);
-	public static DoubleSolenoid CLIMB_R = new DoubleSolenoid(2, 3);
-	public static Compressor compressor = new Compressor(1);
+    // Sensors
+    public static ADIS16448_Sensor SENSOR_HAT = new ADIS16448_Sensor(); // This HAT has a many, many sensors on it
+    
+    // Pneumatics - give them some slack, their just kinda bloated
+    public static DoubleSolenoid CLIMB_L = new DoubleSolenoid(0, 1);
+    public static DoubleSolenoid CLIMB_R = new DoubleSolenoid(2, 3);
+    public static Compressor compressor = new Compressor(1);
 
-	// Camera Settings
-	public static final int
-	cam0Width = 160,
-	cam0Height = 120,
-	cam0FPS = 8,
-	cam0Compression = 75, // 75% 
-	cam0Exposure = 5; // These settings seemed to work best under use of one camera
+    // Camera Settings
+    public static final int
+    cam0Width = 160,
+    cam0Height = 120,
+    cam0FPS = 8,
+    cam0Compression = 75, // 75% 
+    cam0Exposure = 5; // These settings seemed to work best under use of one camera
 
-	// Start Camera Stream Systems
-	//public static Camera camera0 = new Camera("Camera0", 0 , cam0Width, cam0Height, cam0FPS, cam0Exposure);
-	//TODO get the camera working maybe? XDD
+    // Start Camera Stream Systems
+    //public static Camera camera0 = new Camera("Camera0", 0 , cam0Width, cam0Height, cam0FPS, cam0Exposure);
+    //TODO get the camera working maybe? XDD
 
-	// Controller Port Numbers
-	public static final int CONTROLLER1 = 0;
+    // Controller Port Numbers
+    public static final int CONTROLLER1 = 0;
 }
