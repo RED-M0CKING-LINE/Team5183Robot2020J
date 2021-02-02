35bc1cadac157e6fe4d2f4014faa4bf51efa8182
Ethan Ashley <redmockingline@gmail.com>
2021-02-02 15:20:51 -0500

    modified:   Misc/shuffleboardSettings.json modified:   Team5183Robot2020J/src/main/java/frc/robot/Robot.java 	deleted:    Team5183Robot2020J/src/main/java/frc/robot/RobotMap.java 	deleted:    Team5183Robot2020J/src/main/java/frc/robot/hardware/ADXRS450_SPIGyro.java 	deleted:    Team5183Robot2020J/src/main/java/frc/robot/hardware/XboxCustom.java 	deleted:    Team5183Robot2020J/src/main/java/frc/robot/subsystems/DriveTrain.java 	deleted:    Team5183Robot2020J/src/main/java/frc/robot/subsystems/DriverStation.java 	deleted:    Team5183Robot2020J/src/main/java/frc/robot/util/Curves.java 	deleted:    Team5183Robot2020J/src/main/java/frc/robot/util/Kinematics.java

--- Team5183Robot2020J/src/main/java/frc/robot/Robot.java
+++ Team5183Robot2020J/src/main/java/frc/robot/Robot.java
@@ -1,66 +1,68 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
-// Robot Program Created By Ethan Ashley, AKA RedMockingLine on GitHub
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-
-import frc.robot.RobotMap;
-import frc.robot.hardware.XboxCustom;
-import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.DriverStation;
-
-/** This is where the main program flow is controled. */
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Spark;
+/**
+ * Add your docs here.
+ */
 public class Robot extends TimedRobot {
-    public static XboxCustom ctrl = new XboxCustom(RobotMap.CONTROLLER1);
-
+    XboxController xbox = new XboxController(0);
+    Spark motor_l = new Spark(2);
+    Spark motor_r = new Spark(1);
+    Spark motor_il = new Spark(4);
+    Spark motor_ir = new Spark(5);
+    public void drive() {
+        double x = xbox.getX();
+        double y = xbox.getY();
+        motor_l.set(y);
+        motor_r.set(y);
+        motor_ir.set(y);
+        motor_il.set(y);
+    }
     @Override
     public void robotInit() {
-        DriveTrain.inititalize();
-        DriverStation.initialize();
+        motor_l.enableDeadbandElimination(true);
+        motor_r.enableDeadbandElimination(true);
+        motor_l.setSafetyEnabled(true);
+        motor_r.setSafetyEnabled(true);
     }
-  
     @Override
     public void robotPeriodic() {
-        DriverStation.update();
+
     }
-  
     @Override
     public void autonomousInit() {
-        DriveTrain.move(.3, .3);
-        Timer.delay(1);
-        DriveTrain.stop();
-        
-    } 
 
+    }
     @Override
     public void autonomousPeriodic() {
 
     }
-  
-    @Override
+    @Override 
     public void teleopInit() {
-        DriveTrain.stop();  // incase the robot crashes while switching, or takes too long, the bot will stop.
+
     }
-  
     @Override
-    public void teleopPeriodic() {DriveTrain.teleopDrive(ctrl.getRBumperState());}
-  
+    public void teleopPeriodic() {
+
+    }
+    public void testPeriodic() {
+
+    }
     @Override
-    public void testInit() {}
-  
-    @Override
-    public void testPeriodic() {}
+    public void startCompetition() {
+
+    }
 
     @Override
-    public void simulationInit() {}
+    public void endCompetition() {
 
-    @Override
-    public void simulationPeriodic() {}
+    }
 }
